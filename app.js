47bbe866a7b238dfc54a5e69ec38bdd7bbbbb8a2
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-10 10:57:07 -0600

    completed exercise 1-3

--- app.js
+++ app.js
@@ -5,15 +5,64 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
+let a = 20;
+let b = 4;
+let add= a+b;
+let minus= a-b;
+let multiply= a*b;
+let divide= a/b;
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(divide);
 
 // YOUR CODE HERE
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 
+console.log(num+str);
+console.log(num+str2);
+console.log(firstName+num);
+console.log(num+isPresent);
+console.log(isPresent+str);
+console.log(firstName+lastName);
 // YOUR CODE HERE
-
+// What is the value of: num + str? 1111
+// What is the value of: num + str2? 11eleven
+// What is the value of: num + isPresent?12
+// What is the value of: firstName + num?Frodo11
+// What is the value of: isPresent + str?true11
+// What is the value of: firstName + lastName?FrodoBaggins
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
 
+console.log(val==str3)
+console.log(val===str3)
+console.log(!isPresent2);
+console.log("eleven" == str4 && val >= str3)
+console.log(!isPresent2 || isPresent2)
+console.log(0 == false)
+console.log(0===false)
+console.log(0 != false)
+console.log(0 !== false)
 // YOUR CODE HERE
+// What is the value of: val == str3? True
+// What is the value of: val === str3? False
+// What is the value of: !isPresent2? True
+// What is the value of: ("eleven" == str4 && val >= str3)?False
+// What is the value of: (!isPresent2 || isPresent2)? True
+// What is the value of: 0 == false? True
+// What is the value of: 0 === false? False
+// What is the value of: 0 != false? False
+// What is the value of 0 !== false? True
